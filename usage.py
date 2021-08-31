ffbdfc792511e271e55a7ead0676093d90fecbc8
c <you@example.com>
2021-08-31 00:16:05 +0000

    Automatically create inputs for visuals based on demand-trend-reps output.

--- usage.py
+++ usage.py
@@ -6,6 +6,7 @@ Created on Tue Jul 13 18:21:38 2021
 @author: craig
 """
 import by_demandgroup as dep
+import os
 
 runs = {'without-peak-hold': "/home/craig/runs/rand-runs-demand-trends/base-testdata-v7/DemandTrends.txt",
         'with-peak-hold': {'max_path': '/home/craig/runs/visualizing_demand/computed_maxes.xlsx',
@@ -24,4 +25,36 @@ little_run = {"A": "/home/craig/runs/test-run/testdata-v7-bog/DemandTrends.txt",
 
 #add_periods(load_trends("/home/craig/runs/big_test/base-testdata-v7/DemandTrends.txt"), "/home/craig/runs/big_test/base-testdata-v7.xlsx")
 
-dep.delta_table(big_run, "/home/craig/runs/visualizing_demand/")
+#dep.delta_table(big_run, "/home/craig/runs/visualizing_demand/")
+
+#In trends, two folders, one for A and one for B. done in Clojure
+#assume cuts the same between folders, so first, get a list of folders
+#in the directory
+
+
+#given a directory (with folders for each scenario), make an input map
+#where the keys are the names of the trend files and the values are a run-map with max_path and
+#run_info is from all folders within trends
+#combined trends get outputted after the visual, need to suck these back into r.
+
+
+def cut_map(trend_path, max_path):
+    """Generate the run map to send to delta_table. trend_path should contain only directories, and each directory holds a list of demand trends.  each directory should contain the same number and file names for the demand trends."""
+    subfolders = [ f for f in os.scandir(trend_path) if f.is_dir() ]
+    first_folder=subfolders[0].path
+    res={}
+    for f in os.scandir(first_folder):
+        upper_dict={}
+        run_dict={}
+        for run in subfolders:
+            #need to figure out how to loop here.
+            #just the file name
+            run_dict[run.name]=run.path + "/" + f.name
+        name = os.path.splitext(f.name)[0]
+        upper_dict['max_path']= max_path
+        upper_dict['run_info']=run_dict
+        res[name]=upper_dict
+    return res
+
+max_path = '/home/craig/runs/visualizing_demand/computed_maxes.xlsx'
+cut_map("/home/craig/runs/cut-trends/trends/", max_path)
